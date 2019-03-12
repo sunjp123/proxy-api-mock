ef71e82f1dd29e9f09244d11e6e0072862973426
孙建平 <sunjianping@sunjianpingdeMacBook-Pro.local>
2019-03-12 21:49:02 +0800

    add proxy

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const handler = require('serve-handler')
-const http = require('http')
-const conf = require('./conf')
+const handler = require('serve-handler');
+const httpProxy = require('http-proxy');
+const http = require('http');
+const conf = require('./conf');
+const proxy = httpProxy.createProxyServer({})
 const server = http.createServer((req,res)=>{
    if(conf[req.url]){
        req.url = conf[req.url]
+       return handler(req,res)
    }
-   return handler(req,res)
+   proxy.web(req, res, { target: 'http://happyrecord.cn' });
 })
 
 server.listen('3005',()=>{
